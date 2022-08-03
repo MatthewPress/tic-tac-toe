dd0773d777a1d8d779e8d2b11573df53c8c57203
MatthewPress <press.matt14@gmail.com>
2022-08-03 15:01:16 -0600

    feat: switch starting player on reset

--- scripts/game.js
+++ scripts/game.js
@@ -11,6 +11,15 @@ class Game {
     this.currentTurn = this.startingTurn;
   }
 
+  changeStartingTurn() {
+    if (this.startingTurn === this.players[0]) {
+      this.startingTurn = this.players[1];
+    }
+    else if (this.startingTurn === this.players[1]) {
+      this.startingTurn = this.players[0];
+    }
+  }
+
   trackGame() {
 
   }
