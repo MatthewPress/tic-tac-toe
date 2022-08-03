d9e315f7402711ab386000f8bdfc62171c4f1ec8
MatthewPress <press.matt14@gmail.com>
2022-08-03 14:46:39 -0600

    chore: clean up for next branch

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,10 @@
-// Iteration 0: On First load
-  // Determine (game method) whose turn it is
-  // Output (querySelector) player's turn
-
 // ***** Selectors *****
 var playerOneToken = document.querySelector('.player-one-token');
 var playerOneWins = document.querySelector('.player-one-wins');
 var playerTwoToken = document.querySelector('.player-two-token');
 var playerTwoWins = document.querySelector('.player-two-wins');
 var turnDisplay = document.querySelector('.game-turn-display');
+
 // ***** Data *****
 var currentGame = createGame();
 
@@ -19,13 +16,11 @@ function createGame() {
   var player1 = new Player(0, "🟩");
   var player2 = new Player(1, "🟦");
   var createdGame = new Game();
-  console.log (player1, player2, createdGame);
 
   createdGame.players.push(player1);
   createdGame.players.push(player2);
 
   createdGame.setTurn();
-  console.log (createdGame, createdGame.players);
 
   return createdGame;
 }
