07a126a4ce7ec27cba5909b215f3ac11bfbd3bc7
MatthewPress <press.matt14@gmail.com>
2022-08-03 03:47:21 -0600

    chore: pseudocode game on load

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
+// Iteration 0: On First load
+  // two players are created
+  // one game is created
+  // protect both inside and on load event listener and handler
+  // players are put in the game object instance
+  // Output (querySelector) players and win count
+  // Determine (game method) whose turn it is
+  // Output (querySelector) player's turn
+
 // ***** Selectors *****
 
 // ***** Data *****
